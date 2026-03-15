c5f0cf134028df42a84319e7f906abb1fae6d571
Hk Prashant Singh <donkille1111@gmail.com>
2026-03-16 00:07:04 +0530

    Update MainActivity.java
    Fix

--- HK-Browser/app/src/main/java/com/hk/MainActivity.java
+++ HK-Browser/app/src/main/java/com/hk/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
     private LinearLayout loaderLayout;
     private TextView statusText;
     private AlertDialog internetDialog; 
-    private final String TARGET_URL = "https://hk-love.netlify.app/";
+    private final String TARGET_URL = "https://hk-mall-16bb9.web.app/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
